c0e2eb704234788d68beed968694a6ef7fcaaef8
Lucas Diego <luucasdiego_12@hotmail.com>
2022-03-25 01:09:34 -0400

    Projeto iniciado

---
+++ src/main/java/com/lucas/pendingtasks/PendingTasksApplication.java
@@ -0,0 +1,13 @@
+package com.lucas.pendingtasks;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PendingTasksApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PendingTasksApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/lucas/pendingtasks/PendingTasksApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lucas.pendingtasks;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PendingTasksApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
