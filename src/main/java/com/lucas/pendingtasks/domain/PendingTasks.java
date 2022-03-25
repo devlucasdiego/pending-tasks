46f55e1dc8feb971f89916e12412e2f45db5c89b
Lucas Diego <luucasdiego_12@hotmail.com>
2022-03-25 01:40:32 -0400

    Annotations

--- src/main/java/com/lucas/pendingtasks/domain/PendingTasks.java
+++ src/main/java/com/lucas/pendingtasks/domain/PendingTasks.java
@@ -1,10 +1,21 @@
 package com.lucas.pendingtasks.domain;
 
+import java.io.Serializable;
 import java.time.LocalDateTime;
 import java.util.Objects;
 
-public class PendingTasks {
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 
+@Entity
+public class PendingTasks implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Integer id;
 	private String titulo;
 	private String descricao;
